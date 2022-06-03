ba9c3d2d597c13b8e0712aa25c8a0db7148a0933
Yujin Won <iamyujinwon@gmail.com>
2022-06-02 23:18:09 -0400

    enable to add and remove

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-{/* <div class="to-do">
-<div class="content">
-  <input class="checkbox" type="checkbox">
-  <div class="task-title"></div>
-</div>
-<i class="fa fa-trash-o fa-lg"></i>
-</div> */}
-
-
-
 let input = document.querySelector('input');
 let task;
-const taskList = document.querySelector("#task-list")
+const taskList = document.querySelector("#task-list");
 
 input.addEventListener('keypress', e => {
   if (e.key === 'Enter') {
     task = input.value;
-    input.value= "";
-    
-    addRow();
+
+    if (task != "") {
+      clearInput();
+      addTask();
+    }
   }
 });
 
-function addRow() {
-  const div = document.createElement('div');
+function clearInput() {
+  input.value= "";
+}
 
-  div.className = 'to-do';
+function addTask() {
+  const div = document.createElement('div');
+  div.setAttribute("class", "to-do");
 
   div.innerHTML = `
     <div class="content">
       <input class="checkbox" type="checkbox">
       <div class="task-title">${task}</div>
     </div>
-    <i class="fa fa-trash-o fa-lg"></i>
+    <i class="fa-solid fa-circle-minus fa-lg"></i>
   `;
 
   taskList.appendChild(div);
+
+  const remover = div.querySelector(".fa-circle-minus");
+  remover.addEventListener("click", e => {
+    const minusButton = e.target;
+    const toDo = minusButton.parentNode;
+    taskList.removeChild(toDo);
+  });
 }
